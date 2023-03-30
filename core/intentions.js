d34809acb61628212a825bbf86a3ccd275ce03c0
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2023-03-30 08:37:33 -0300

    WIP adding context to intentions

---
+++ core/intentions.js
@@ -0,0 +1,21 @@
+import getGPT4Response from '../gpt4.js';
+
+const intentionsContext = [
+  { role: "system", content: `Você é um assistente de IA treinado para identificar a itenção (ação) do comando do usuário correspondente:
+  Exemplos:
+- "Ligar a lâmpada do quarto": ewelink_action
+- "Desligar o ventilador da sala": ewelink_action
+- "Qual a temperatura do ar-condicionado do escritório?": ewelink_action
+- "Ajustar a intensidade da luz da cozinha para 50%": ewelink_action
+- "O que é a capital da França?": general_knowledge
+` }
+];
+async function detectIntent(userInput) {
+  intentionsContext.push({ role: 'user', content: userInput})
+  const gptResponse = await getGPT4Response(prompt);
+  intentionsContext.push({role: "system", content: gptResponse})
+  console.log(intentionsContext)
+  return { gptResponse, userInput };
+}
+
+export default detectIntent;
