c4bed93355abf7eef5e69939e4357daa8ac123b6
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2023-03-30 07:47:25 -0300

    cleaning the code

--- gpt4.js
+++ gpt4.js
@@ -3,15 +3,9 @@ const axios = window.axios;
 import { API_KEY } from './apikey.js';
 
 const messages = [
-  // { role: "system", content: "Você é um assistente de IA treinado para responder perguntas." },
-  {
-    role: 'system',
-    content:
-      'Você é uma pessoa extremamente atenciosa e gentil, se comunica falando girias.',
-  },
+  // { role: "system", content: "Você é um assistente de IA treinado para responder perguntas." }
 ];
 async function getGPT4Response(prompt) {
-  // const url = 'https://api.openai.com/v1/engines/gpt-3.5-turbo/completions';
   messages.push({ role: 'user', content: prompt });
   const url = 'https://api.openai.com/v1/chat/completions';
 
@@ -19,14 +13,6 @@ async function getGPT4Response(prompt) {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${API_KEY}`,
   };
-
-  // const data = {
-  //   prompt: prompt,
-  //   max_tokens: 50,
-  //   n: 1,
-  //   stop: null,
-  //   temperature: 0.7,
-  // };
 
   const data = {
     model: 'gpt-3.5-turbo',
